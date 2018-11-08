bcb04ff9fe7cb0fd03062fb0600651bbae6056c1
misocho <misochobrian@gmail.com>
2018-11-08 08:09:52 -0500

    First commit of th python flask project

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'The article body',
+            'author' : 'Brian Misocho',
+            'create_date':'11-08-2018'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : 'The article body',
+            'author' : 'Brian Misocho',
+            'create_date':'11-08-2018'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : 'The article body',
+            'author' : 'Brian Misocho',
+            'create_date':'11-08-2018'
+        },
+    ]
+    return articles
